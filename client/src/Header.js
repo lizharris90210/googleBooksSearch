312002a28a0239bde9d1e5ce83d266755be4467e
Liz Harris <lizharris90210@gmail.com>
2020-01-07 21:38:24 -0500

    backend working, some db issues

---
+++ client/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            {/* <i class="fas fa-book"></i> book icon from somewhere */}
+            <h1>Book Cards</h1>
+        </header>
+    )
+}
+
+export default Header
